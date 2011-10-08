c47e0e7cb5a5b52da3373337a60f0a180c9b69d9
grey.c.rus <grey.c.rus@localhost>
2011-10-08 23:04:12 +0400

    del errors mobicq

--- src/gc/ann/encog/ForexEncogServer.java
+++ src/gc/ann/encog/ForexEncogServer.java
@@ -23,53 +23,100 @@ import org.encog.neural.networks.training.Train;
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 import org.encog.persist.EncogDirectoryPersistence;
 
-
 public class ForexEncogServer {
 	public static double IDEAL[][] = { { 0.0 } };
 	public static double INPUT[][] = { { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
 			0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 } };;
-	public static String smbl = "";
-	public static String DoIt = "";
+	public static String smbl = "",tf="",DoIt = "";
 
-	 public static void main(String[] args) throws IOException {
-		    System.out.println("Welcome to Server side");
-		    BufferedReader in = null;
-		    PrintWriter    out= null;
+	public static void main(String[] args) throws IOException {
+		System.out.println("Welcome to Server side");
+		BufferedReader in = null;
+		PrintWriter out = null;
+		BasicNetwork network;
+		ServerSocket servers = null;
+		Socket fromclient = null;
 
-		    ServerSocket servers = null;
-		    Socket       fromclient = null;
+		// create server socket
+		try {
+			servers = new ServerSocket(7777);
+		} catch (IOException e) {
+			System.out.println("Couldn't listen to port 7777");
+			System.exit(-1);
+		}
 
-		    // create server socket
-		    try {
-		      servers = new ServerSocket(7777);
-		    } catch (IOException e) {
-		      System.out.println("Couldn't listen to port 7777");
-		      System.exit(-1);
-		    }
+		try {
+			System.out.print("Waiting for a client...");
+			fromclient = servers.accept();
+			System.out.println("Client connected");
+		} catch (IOException e) {
+			System.out.println("Can't accept");
+			System.exit(-1);
+		}
 
-		    try {
-		      System.out.print("Waiting for a client...");
-		      fromclient= servers.accept();
-		      System.out.println("Client connected");
-		    } catch (IOException e) {
-		      System.out.println("Can't accept");
-		      System.exit(-1);
-		    }
+		in = new BufferedReader(new InputStreamReader(
+				fromclient.getInputStream()));
+		out = new PrintWriter(fromclient.getOutputStream(), true);
+		String input, output;
 
-		    in  = new BufferedReader(new 
-		     InputStreamReader(fromclient.getInputStream()));
-		    out = new PrintWriter(fromclient.getOutputStream(),true);
-		    String         input,output;
-
-		    System.out.println("Wait for messages");
-		    while ((input = in.readLine()) != null) {
-		     if (input.equalsIgnoreCase("exit")) break;
-		     out.println("S ::: "+input);
-		     System.out.println(input);
-		    }
-		    System.out.println("Server shutdown...");
-		    out.close();
-		    in.close();
-		    fromclient.close();
-		    servers.close();
-		  }}
+		System.out.println("Wait for messages");
+		while ((input = in.readLine()) != null) {
+			if (input.equalsIgnoreCase("!Exit"))
+				break;
+			if (input.startsWith("ENCOG,")) {
+				// format ENCOG,PAIR,TF,DATA1,DATA2,...
+				String[] result = input.split(",");
+				if(result.length<3)
+				{
+					out.println("Error format " );
+					continue;
+				}
+				smbl = result[1]; tf=result[2];
+				String dataDir = "c:\\Program Files\\MetaTrader 5\\MQL5\\Files\\";
+				final File networkFile = new File(dataDir, "Easy_"+smbl+"_"+tf+".eg");
+				try {
+					network = (BasicNetwork) EncogDirectoryPersistence
+							.loadObject(networkFile);
+					} 
+				catch (Exception e) 
+					{
+						out.println("Network "+dataDir+ "\\Easy_"+smbl+"_"+tf+".eg"+" open failed " );
+					 continue;
+					}
+				if(network.getInputCount()!=(result.length-3))
+				{ 
+					out.println("Size input ("+(result.length-3)+") != network input size ("+network.getInputCount() +")");
+					continue;
+				}
+				for (int x = 2; x < result.length; x++)
+					INPUT[0][x - 1] = Double.valueOf(result[x]);
+				BasicMLDataSet inputSet = new BasicMLDataSet(INPUT,
+						IDEAL);
+				for (MLDataPair pair : inputSet) {
+					final MLData outputEnvog = network.compute(pair
+							.getInput());
+					DoIt = "None";
+					if (-0.66 > outputEnvog.getData(0))
+						DoIt = "!Sell " + smbl;
+					else if (-0.33 > outputEnvog.getData(0))
+						DoIt = "!CloseBuy " + smbl;
+					else if (0.33 > outputEnvog.getData(0))
+						DoIt = "None " + smbl;
+					else if (0.66 < outputEnvog.getData(0))
+						DoIt = "!Buy " + smbl;
+					else if (0.33 < outputEnvog.getData(0))
+						DoIt = "CloseSell " + smbl;
+					out.println(DoIt);
+				}
+				//delete(network);
+			} else
+				out.println("S ::: " + input);
+			System.out.println(input);
+		}
+		System.out.println("Server shutdown...");
+		out.close();
+		in.close();
+		fromclient.close();
+		servers.close();
+	}
+}
